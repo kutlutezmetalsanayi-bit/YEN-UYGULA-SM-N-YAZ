32478137f3f00e18106d9b7aad78a8b474adb029
HAKAN <kutlutezmetalsanayi@gmail.com>
2026-08-28 10:20:26 +0300

    Target Android 16 for Play readiness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.kutlutezmetalsanayi.banasoyle"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.kutlutezmetalsanayi.banasoyle"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures {

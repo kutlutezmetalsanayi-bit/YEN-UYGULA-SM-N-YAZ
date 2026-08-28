4c343d63508c8b3bd3f65282d41c99b8696042bd
HAKAN <kutlutezmetalsanayi@gmail.com>
2026-08-28 10:36:59 +0300

    Remove unused Android imports before build

--- app/src/main/java/com/kutlutezmetalsanayi/banasoyle/MainActivity.kt
+++ app/src/main/java/com/kutlutezmetalsanayi/banasoyle/MainActivity.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
